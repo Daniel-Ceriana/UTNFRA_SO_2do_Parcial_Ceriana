6bda02bb472031fb10f78f96bdc3c82bd27b5e85
danielceriana <ceriana@segundoParcial>
2024-07-05 14:50:10 +0000

    buffer

---
+++ buffer/RTA_Examen_20240705/Punto_D.sh
@@ -0,0 +1,18 @@
+ansible-galaxy role init directorios
+cd directorios/
+vim tasks/main.yml
+vim tests/test.yml
+ansible-playbook -i tests/inventory tests/test.yml
+ansible-galaxy role init archivos
+cd archivos/
+vim alumno.j2
+cd vars/
+vim main.yml
+vim ansible.cfg
+vim main.yml
+cd ..
+ansible-playbook -i tests/inventory tests/test.yml
+vim tests/test.yml
+vim ansible.cfg
+vim tasks/main.yml
+vim vars/main.yml
